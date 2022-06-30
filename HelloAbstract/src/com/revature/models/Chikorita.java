0ebaf0f0aa0777b754af7101f5a581cdf4b712fd
cruzgsworks <jigs.gisc@gmail.com>
2022-06-30 13:30:10 -0700

    minor

---
+++ HelloAbstract/src/com/revature/models/Chikorita.java
@@ -0,0 +1,24 @@
+package com.revature.models;
+
+// this concrete class extends pokemon abstract class
+// more specific of generic pokemon abstract class
+// pokemon also implements creature interface giving it all access to creature and pokemon fields.
+public class Chikorita extends Pokemon {
+
+	// eat method from creature interface
+	@Override
+	public void eat(String food) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// fight method from pokemon abstract class
+	@Override
+	public void fight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
